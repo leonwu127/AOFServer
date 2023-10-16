6ce28b1a36757a59b82ddc1c53a7e7e26106529c
leonwu127 <leonwu127@gmail.com>
2023-10-16 16:21:21 +0100

    add parent folder git ignore

--- ArmyServer.Tests/AuthenticationControllerTests.cs
+++ ArmyServer.Tests/AuthenticationControllerTests.cs
@@ -1,104 +1,46 @@
-﻿using ArmyServer.Auth;
+﻿using Xunit;
+using Moq;
+using System.Net;
+using System.IO;
 using ArmyServer.Controllers;
 using ArmyServer.Models;
-using System.Net;
-using System.Text;
-using Xunit;
 
 namespace ArmyServer.Tests
 {
-    public class ArmyServerTests : IDisposable
+    public class AuthenticationControllerTests
     {
-        private const string TestUrl = "http://localhost:12345/";
-        private string _gameTitle = GameTitle.ArmyofTactics.ToString();
-        private readonly HttpListener _listener;
-        private readonly HttpClient _client;
+        private readonly Mock<HttpListenerRequest> _requestMock;
+        private readonly Mock<HttpListenerResponse> _responseMock;
         private readonly AuthenticationController _controller;
-        public ArmyServerTests()
-        {
-            // Initialize the controller. If the controller has dependencies, mock them here.
-            _listener = new HttpListener();
-            _listener.Prefixes.Add(TestUrl);
-            _listener.Start();
 
-            _client = new HttpClient();
-            _controller = new AuthenticationController();
+        public AuthenticationControllerTests()
+        {
+            // Mocking HttpListenerRequest and HttpListenerResponse, more setup might be needed depending on your tests.
+            _requestMock = new Mock<HttpListenerRequest>();
+            _responseMock = new Mock<HttpListenerResponse>();
+
+            // If AuthenticationController has dependencies, they need to be mocked and passed to the constructor as well.
+            _controller = new AuthenticationController(/* dependencies */);
         }
 
         [Fact]
-        public async Task GuestRegister_ShouldReturnToken()
+        public void GuestRegister_ValidRequest_CreatesAccount()
         {
             // Arrange
-            var requestUrl = $"{TestUrl}register";
-
-            // Start a task to handle the incoming request
-            var handleRequestTask = Task.Run(async () =>
-            {
-                var listenerContext = await _listener.GetContextAsync();
-                _controller.GuestRegister(listenerContext.Request, listenerContext.Response);
-                listenerContext.Response.Close();
-            });
+            // Set up your mocks, including the request and response objects and any methods they should call.
+            // For instance, if your method relies on the request's InputStream, you'd set that up here.
 
             // Act
-            var httpResponse = await _client.GetAsync(requestUrl);
-
-            // Wait for the request handling task to complete
-            await handleRequestTask;
+            // Call your method under test.
+            _controller.GuestRegister(_requestMock.Object, _responseMock.Object);
 
             // Assert
-            var responseContent = await httpResponse.Content.ReadAsStringAsync();
-            Assert.Contains("token", responseContent); // Example assertion
-
+            // Verify the expected results, such as checking the response object for expected status codes or headers.
+            // For instance, if your method sets a certain status code in the response upon success, you'd check that here.
         }
 
-        [Fact]
-        public async Task Login_ShouldReturnToken()
-        {
-            // Arrange
-            var requestUrl = $"{TestUrl}{_gameTitle}/login";
-            var requestBodyContent = new
-            {
-                Provider = new Dictionary<string, AuthCredential>
-                {
-                    ["GameCenter"] = new AuthCredential
-                    {
-                        PlayerId = "testPlayer",
-                        Token = "testToken"
-                    }
-                }
-            };
-
-            var requestBodyJson = System.Text.Json.JsonSerializer.Serialize(requestBodyContent);
-            var requestBody = new StringContent(requestBodyJson, Encoding.UTF8, "application/json");
-
-            // Set headers
-            _client.DefaultRequestHeaders.Add("platform", "iOS");
-
-            // Start a task to handle the incoming request
-            var handleRequestTask = Task.Run(async () =>
-            {
-                var listenerContext = await _listener.GetContextAsync();
-                _controller.Login(listenerContext.Request, listenerContext.Response);
-                listenerContext.Response.Close();
-            });
-
-            // Act
-            var httpResponse = await _client.PostAsync(requestUrl, requestBody);
-
-            // Wait for the request handling task to complete
-            await handleRequestTask;
-
-            // Assert
-            var responseContent = await httpResponse.Content.ReadAsStringAsync();
-            Assert.Contains("token", responseContent); // Example assertion
-        }
-
-        public void Dispose()
-        {
-            _listener.Stop();
-            _listener.Close();
-            _client.Dispose();
-        }
-
+        // More tests...
+        // You would create additional test methods for other scenarios, such as invalid requests, exceptions, etc.
     }
+
 }
