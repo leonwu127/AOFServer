a978234c91dc23dedd7551f4e49db12482bafe1a
leonwu127 <leonwu127@gmail.com>
2024-03-13 13:45:41 +0000

    add startup file to use ASP.NET

---
+++ TinyGameServer/Startup.cs
@@ -0,0 +1,28 @@
+﻿using TinyGameServer.Data;
+using TinyGameServer.Models;
+
+namespace TinyGameServer;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSingleton<IDataRepository<string, Player>, PlayerRepository>();
+    }
+    
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
